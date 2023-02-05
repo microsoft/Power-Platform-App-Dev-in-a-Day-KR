36c5e945cb66610eec02a18b6e57544ae683d1c0
Justin Yoo <justin.yoojh@gmail.com>
2023-02-05 03:21:47 +0000

    devcontainer 설정 업데이트

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
 ## Install additional apt packages
-sudo apt-get update && \
-    sudo apt-get install -y dos2unix libsecret-1-0
+sudo apt-get update \
+    && sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils \
+    && sudo apt-get clean -y && sudo rm -rf /var/lib/apt/lists/*
 
 ## Configure git
 git config --global pull.rebase false
